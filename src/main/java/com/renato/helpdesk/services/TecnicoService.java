b5e0625fc583a957417217e644ea20e8c7f89291
rccesar <renato11cesar@hotmail.com>
2022-04-04 22:31:17 -0300

    Tratamento de Exceção

--- src/main/java/com/renato/helpdesk/services/TecnicoService.java
+++ src/main/java/com/renato/helpdesk/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.renato.helpdesk.services;
 
 
 import com.renato.helpdesk.domain.Tecnico;
+import com.renato.helpdesk.exceptions.ObjectnotFoundException;
 import com.renato.helpdesk.repositories.TecnicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,8 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado! Id " + id ));
+
 
     }
 }
